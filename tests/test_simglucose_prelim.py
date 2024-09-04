cf2b027a27a959169c1a64fe5870714a49d0585e
Alex Donze <alex@decyphir.com>
2024-09-04 15:34:28 +0200

    simglucose wip

--- tests/test_simglucose_prelim.py
+++ tests/test_simglucose_prelim.py
@@ -15,7 +15,6 @@ tester.model_id = model
 # creates one mock run to begin with
 
 trace_idx = 0
-
 trace = []
 total_reward = 0
 for i in range(10):
@@ -33,39 +32,39 @@ for i in range(10):
 
 
 trace_idx = tester.add_trace(trace)
-new_record = pd.DataFrame({'trace_idx':trace_idx, 'env_name':tester.env_name,'model_name': [tester.model_id],'seed': [trace_idx], 'total_reward': [total_reward]})
+new_record = pd.DataFrame({'trace_idx':trace_idx, 
+                           'env_name':tester.env_name,
+                           'model_name': [tester.model_id],
+                           'seed': [trace_idx], 'total_reward': [total_reward]})
 
 if tester.evals is None:
     tester.evals = new_record
 else:
     tester.evals = pd.concat([tester.evals, new_record])
-       
 
 print("trace",trace)
 tester.signals_names = ["BG"]
 
+spec_prompt = "alw BG[t] > 100"
+plot_prompt = "BG"
+[st, fig] = update_plot(spec_prompt,  plot_prompt)
 
 with gr.Blocks(fill_height=True) as sg_gui:
-    textbox_specs = gr.Textbox(label="Specifications",lines=10, interactive=True)         
-    textbox_plot_prompt = gr.Textbox(label="Plot layout",lines=5, interactive=True)            
+    textbox_bg_specs = gr.Textbox(label="Specifications",lines=10, interactive=True)         
+    textbox_bg_plot_prompt = gr.Textbox(label="Plot layout",lines=5, interactive=True)            
     with gr.Row():
-            button_eval = gr.Button("Eval Specs")                   
-            button_plot = gr.Button("Update Plot")
-    dataframe_sg_evals = gr.DataFrame(row_count= (8, 'dynamic'),value= tester.evals,  interactive= False)
+            button_bg_eval = gr.Button("Eval Specs")                   
+            button_bg_plot = gr.Button("Update Plot")
+    status_bg= gr.Textbox(label="Status", interactive=False)
     with gr.Tabs():
         with gr.Tab(label="Evaluations"):
-            dataframe_evals = gr.DataFrame(row_count= (8, 'dynamic'), interactive= False)
+            dataframe_bg_evals = gr.DataFrame(row_count= (8, 'dynamic'),value= tester.evals, interactive= False)
         with gr.Tab(label="Plot"):
             with gr.Group():
-                fig = gr.Plot()
-button_plot.click(update_plot, [textbox_specs,textbox_plot_prompt], [fig, textbox_status])    
-button_eval.click(eval_stl, [textbox_specs, dataframe_evals], [textbox_status, dataframe_evals])
+                fig_bg = gr.Plot()
+    button_bg_plot.click(update_plot, [textbox_bg_specs, textbox_bg_plot_prompt], [status_bg, fig])    
+    button_bg_eval.click(eval_stl,    [textbox_bg_specs, dataframe_bg_evals], [status_bg, dataframe_bg_evals])
     
-
-
-
-
 if __name__ == '__main__':
-    #[specs.value, plot_prompt.value, status.value]  = callback_env(env)
     sg_gui.launch()
     
