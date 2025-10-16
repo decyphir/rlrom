90d4ec0cca12fbd7c68aa269a061037c39867855
Alexandre Donze <alexandre.donze@univ-grenoble-alpes.fr>
2025-10-16 09:53:56 +0200

    collect res with polars and plot enveloppe

--- src/rlrom/plots.py
+++ src/rlrom/plots.py
@@ -6,6 +6,7 @@ import itertools
 import re
 import matplotlib.pyplot as plt
 import rlrom.utils as utils
+import polars as pl
 
 
 def get_layout_from_string(signals_layout):
@@ -75,29 +76,50 @@ def get_fig(envs, signals_layout, tr_idx=0):
         
     return fig, status
 
-def plot_training_logs(data, ax=None, label=None, color='gray', linestyle='-'):
-# assumes data is a list of sync logs with steps and values fields    
-    
-    steps = data[0].get('steps')
-    mean_values = utils.get_mean_values(data)
-    min_values = utils.get_lower_values(data)
-    max_values = utils.get_upper_values(data)    
-
+def plot_enveloppe(steps, mean_val, min_val, max_val, 
+                      ax=None, label=None, color='gray', linestyle='-'):
     if ax is None:
         fig, ax = plt.subplots(figsize=(8, 4))
         ax.grid(True)
     if label is None:    
-        ax.plot(steps, mean_values, color=color,linestyle=linestyle)
+        ax.plot(steps, mean_val, color=color,linestyle=linestyle)
     else:
-        ax.plot(steps, mean_values,label=label, color=color,linestyle=linestyle)
+        ax.plot(steps, mean_val,label=label, color=color,linestyle=linestyle)
 
-    ax.plot(steps, min_values, color=color, linestyle=linestyle)
-    ax.plot(steps, max_values, color=color,linestyle=linestyle)
+    ax.plot(steps, min_val, color=color, linestyle=linestyle)
+    ax.plot(steps, max_val, color=color,linestyle=linestyle)
 
-    ax.fill_between(steps, min_values, max_values, color=color, alpha=0.25, label='Min-Max Range')
+    ax.fill_between(steps, min_val, max_val, color=color, alpha=0.25, label='Min-Max Range')
     ax.legend()
     ax.set_xlabel('Training Steps')
-    ax.set_ylabel('Average cumulated reward')
+    ax.set_ylabel('Value')
 
 
     return ax
+
+def plot_df_enveloppe(df, feature, ax=None, label=None, color='gray', linestyle='--'):
+# assumes df is a a dataframe df of (steps,feature) concat vertically with label
+    df_enveloppe = utils.get_df_mean_min_max_val(df, feature)
+    steps = df_enveloppe['steps'].to_numpy()
+        
+    feature_mean = f'{feature}_mean'
+    feature_min = f'{feature}_min'
+    feature_max = f'{feature}_max'
+
+    mean_val = df_enveloppe[feature_mean].to_numpy()
+    min_val = df_enveloppe[feature_min].to_numpy()
+    max_val = df_enveloppe[feature_max].to_numpy()
+
+    return plot_enveloppe(steps, mean_val, min_val, max_val, 
+                      ax=ax, label=label, color=color, linestyle=linestyle)
+
+def plot_tb_training_logs(data, ax=None, label=None, color='gray', linestyle='-'):
+# assumes data is a list of sync logs with steps and values fields  PROBABLY DEPRECATED
+    
+    steps = data[0].get('steps')
+    mean_val = utils.get_mean_values(data)
+    min_val = utils.get_lower_values(data)
+    max_val = utils.get_upper_values(data)    
+
+    return plot_enveloppe(steps, mean_val, min_val, max_val, 
+                      ax=ax, label=label, color=color, linestyle=linestyle)
