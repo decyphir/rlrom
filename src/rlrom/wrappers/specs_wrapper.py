bfef02fb32620f4c2676c25a5cf7e3200bbc3e55
Jon Goikoetxea <jon.goikoetxea.m@gmail.com>
2026-07-24 19:40:47 +0200

    Read flatten_obs option from cfg_specs instead of cfg

--- src/rlrom/wrappers/specs_wrapper.py
+++ src/rlrom/wrappers/specs_wrapper.py
@@ -12,7 +12,7 @@ def wrap_env_specs(env,cfg):
         cfg_rm = cfg_specs.get('cfg_rm', None)            
         if cfg_rm is not None:
             env = RewardMachineWrapper(env, cfg_rm)  
-        elif cfg.get("flatten_obs", True):
+        elif cfg_specs.get("flatten_obs", True):
             env = FlattenObservation(env)          
     
     return env
